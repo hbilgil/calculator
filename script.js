289598bd25a718a27c700ae89a566521bbc80eeb
Halil Bilgil <hbilgil@gmail.com>
2022-05-13 21:46:11 +0300

    Edit script.js with UI declarations

---
+++ script.js
@@ -0,0 +1,16 @@
+//UI DECLARATIONS
+
+//buttons
+
+const numberButtons = document.querySelectorAll('[data-number]') //more than 1 button and ALL-SELECTED
+const operationButtons = document.querySelectorAll('[data-operation]') //more than 1 button and ALL-SELECTED
+const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const percentageButton = document.querySelector('[data-percentage]')
+const equalsButton = document.querySelector('[data-equals]')
+
+//text elements
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
